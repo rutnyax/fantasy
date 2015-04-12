e3a915b55a4ab9061877269adf3be28c79cd518b
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-04-12 16:40:35 -0400

    deal with path variable using user_id

--- src/main/java/com/kossyuzokwe/fantasy/controller/UserController.java
+++ src/main/java/com/kossyuzokwe/fantasy/controller/UserController.java
@@ -3,6 +3,7 @@ package com.kossyuzokwe.fantasy.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kossyuzokwe.fantasy.service.UserService;
@@ -17,5 +18,11 @@ public class UserController {
 	public String users(Model model) {
 		model.addAttribute("users", userService.findAll());
 		return "users";
+	}
+	
+	@RequestMapping("/users/{id}")
+	public String userDetail(Model model, @PathVariable String id) {
+		model.addAttribute("user", userService.findOne(id));
+		return "user-detail";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/users.jsp
+++ src/main/webapp/WEB-INF/jsp/users.jsp
@@ -2,6 +2,7 @@
 	pageEncoding="UTF-8"%>
 
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
 
 <table
 	class="table table-striped table-bordered table-hover table-condensed">
@@ -13,7 +14,10 @@
 	<tbody>
 		<c:forEach items="${users}" var="user">
 			<tr>
-				<td>${user.userName}</td>
+				<td><a href='<spring:url value="/users/${user.userId}.html" />'>
+				${user.userName}
+				</a>
+				</td>
 			</tr>
 		</c:forEach>
 	</tbody>
