4fb5be045d76d20325f55548de62a9995e7c92fe
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-04-22 01:52:03 -0400

    Configure the application to deploy and run without errors on heroku. Set the default spring profile to prod in the web.xml. Set the spring profile in the tomcat 7:run configurations to dev. Renamed the development profile from default to dev

--- src/main/java/com/kossyuzokwe/fantasy/service/InitDbService.java
+++ src/main/java/com/kossyuzokwe/fantasy/service/InitDbService.java
@@ -46,72 +46,74 @@ public class InitDbService {
 
 	@PostConstruct
 	public void init() {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		if (roleRepository.findByRoleName("ROLE_ADMIN") == null) {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-		Role roleUser = new Role();
-		roleUser.setRoleName("ROLE_USER");
-		roleRepository.save(roleUser);
+			Role roleUser = new Role();
+			roleUser.setRoleName("ROLE_USER");
+			roleRepository.save(roleUser);
 
-		Role roleAdmin = new Role();
-		roleAdmin.setRoleName("ROLE_ADMIN");
-		roleRepository.save(roleAdmin);
+			Role roleAdmin = new Role();
+			roleAdmin.setRoleName("ROLE_ADMIN");
+			roleRepository.save(roleAdmin);
 
-		User userAdmin = new User();
-		userAdmin.setUserEnabled(true);
-		userAdmin.setUserName("admin");
-		userAdmin.setUserEmail("admin@admin.com");
-		userAdmin.setUserPassword(encoder.encode("admin"));
-		List<Role> roles = new ArrayList<Role>();
-		roles.add(roleUser);
-		roles.add(roleAdmin);
-		userAdmin.setRoles(roles);
-		userRepository.save(userAdmin);
+			User userAdmin = new User();
+			userAdmin.setUserEnabled(true);
+			userAdmin.setUserName("admin");
+			userAdmin.setUserEmail("admin@admin.com");
+			userAdmin.setUserPassword(encoder.encode("admin"));
+			List<Role> roles = new ArrayList<Role>();
+			roles.add(roleUser);
+			roles.add(roleAdmin);
+			userAdmin.setRoles(roles);
+			userRepository.save(userAdmin);
 
-		League premierLeague = new League();
-		premierLeague.setLeagueName("Barclays Premier League");
-		premierLeague.setOwner(userAdmin);
-		leagueRepository.save(premierLeague);
+			League premierLeague = new League();
+			premierLeague.setLeagueName("Barclays Premier League");
+			premierLeague.setOwner(userAdmin);
+			leagueRepository.save(premierLeague);
 
-		Team teamChelsea = new Team();
-		teamChelsea.setTeamName("Chelsea FC");
-		teamChelsea.setUser(userAdmin);
-		teamChelsea.setLeague(premierLeague);
-		teamRepository.save(teamChelsea);
+			Team teamChelsea = new Team();
+			teamChelsea.setTeamName("Chelsea FC");
+			teamChelsea.setUser(userAdmin);
+			teamChelsea.setLeague(premierLeague);
+			teamRepository.save(teamChelsea);
 
-		Player playerHazard = new Player();
-		playerHazard.setPlayerName("Eden Hazard");
-		playerHazard.setTeam(teamChelsea);
-		playerRepository.save(playerHazard);
+			Player playerHazard = new Player();
+			playerHazard.setPlayerName("Eden Hazard");
+			playerHazard.setTeam(teamChelsea);
+			playerRepository.save(playerHazard);
 
-		Player playerTerry = new Player();
-		playerTerry.setPlayerName("John Terry");
-		playerTerry.setTeam(teamChelsea);
-		playerRepository.save(playerTerry);
+			Player playerTerry = new Player();
+			playerTerry.setPlayerName("John Terry");
+			playerTerry.setTeam(teamChelsea);
+			playerRepository.save(playerTerry);
 
-		User userNotAdmin = new User();
-		userNotAdmin.setUserEnabled(true);
-		userNotAdmin.setUserName("test");
-		userNotAdmin.setUserEmail("test@test.com");
-		userNotAdmin.setUserPassword(encoder.encode("test"));
-		List<Role> roles2 = new ArrayList<Role>();
-		roles2.add(roleUser);
-		userNotAdmin.setRoles(roles2);
-		userRepository.save(userNotAdmin);
+			User userNotAdmin = new User();
+			userNotAdmin.setUserEnabled(true);
+			userNotAdmin.setUserName("test");
+			userNotAdmin.setUserEmail("test@test.com");
+			userNotAdmin.setUserPassword(encoder.encode("test"));
+			List<Role> roles2 = new ArrayList<Role>();
+			roles2.add(roleUser);
+			userNotAdmin.setRoles(roles2);
+			userRepository.save(userNotAdmin);
 
-		Team teamArsenal = new Team();
-		teamArsenal.setTeamName("Arsenal");
-		teamArsenal.setUser(userNotAdmin);
-		teamArsenal.setLeague(premierLeague);
-		teamRepository.save(teamArsenal);
+			Team teamArsenal = new Team();
+			teamArsenal.setTeamName("Arsenal");
+			teamArsenal.setUser(userNotAdmin);
+			teamArsenal.setLeague(premierLeague);
+			teamRepository.save(teamArsenal);
 
-		Player playerSanchez = new Player();
-		playerSanchez.setPlayerName("Alexis Sanchez");
-		playerSanchez.setTeam(teamArsenal);
-		playerRepository.save(playerSanchez);
+			Player playerSanchez = new Player();
+			playerSanchez.setPlayerName("Alexis Sanchez");
+			playerSanchez.setTeam(teamArsenal);
+			playerRepository.save(playerSanchez);
 
-		Player playerWalcott = new Player();
-		playerWalcott.setPlayerName("Theo Walcott");
-		playerWalcott.setTeam(teamArsenal);
-		playerRepository.save(playerWalcott);
+			Player playerWalcott = new Player();
+			playerWalcott.setPlayerName("Theo Walcott");
+			playerWalcott.setTeam(teamArsenal);
+			playerRepository.save(playerWalcott);
+		}
 	}
 }
