2ca302b12e167be21bae681973d1c387b48dacba
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-04-29 08:39:35 -0400

    Copy over user service and controller logic

--- src/main/java/com/kossyuzokwe/fantasy/service/UserService.java
+++ src/main/java/com/kossyuzokwe/fantasy/service/UserService.java
@@ -1,7 +1,8 @@
 package com.kossyuzokwe.fantasy.service;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import javax.transaction.Transactional;
 
@@ -14,14 +15,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.kossyuzokwe.fantasy.dao.LeagueRepository;
+import com.kossyuzokwe.fantasy.dao.PasswordResetTokenRepository;
 import com.kossyuzokwe.fantasy.dao.PlayerRepository;
 import com.kossyuzokwe.fantasy.dao.RoleRepository;
 import com.kossyuzokwe.fantasy.dao.TeamRepository;
 import com.kossyuzokwe.fantasy.dao.UserRepository;
+import com.kossyuzokwe.fantasy.dao.VerificationTokenRepository;
+import com.kossyuzokwe.fantasy.model.PasswordResetToken;
 import com.kossyuzokwe.fantasy.model.Player;
-import com.kossyuzokwe.fantasy.model.Role;
 import com.kossyuzokwe.fantasy.model.Team;
 import com.kossyuzokwe.fantasy.model.User;
+import com.kossyuzokwe.fantasy.model.VerificationToken;
 import com.kossyuzokwe.fantasy.util.Constants;
 
 @Service
@@ -46,18 +50,32 @@ public class UserService {
 	private LeagueRepository leagueRepository;
 
 	@Autowired
+	private VerificationTokenRepository verificationTokenRepository;
+
+	@Autowired
+	private PasswordResetTokenRepository resetTokenRepository;
+
+	@Autowired
 	private PasswordEncoder passwordEncoder;
 
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
 
-	public User findOneById(String id) {
-		return userRepository.findOne(id);
+	public User findUserByUserId(String id) {
+		return userRepository.findByUserId(id);
+	}
+
+	public User findUserByUserName(String username) {
+		return userRepository.findByUserName(username);
+	}
+
+	public User findUserByUserEmail(String email) {
+		return userRepository.findByUserEmail(email);
 	}
 
 	public User findOneWithTeamsById(String id) {
-		User user = findOneById(id);
+		User user = findUserByUserId(id);
 		List<Team> teams = teamRepository.findByUser(user, new PageRequest(0,
 				Constants.STANDARD_PAGE_SIZE, Direction.DESC, "teamId"));
 		for (Team team : teams) {
@@ -70,25 +88,76 @@ public class UserService {
 		return user;
 	}
 
-	public void save(User user) {
-		user.setUserEnabled(true);
-		user.setUserPassword(passwordEncoder.encode(user.getUserPassword()));
-		List<Role> roles = new ArrayList<Role>();
-		roles.add(roleRepository.findByRoleName("ROLE_USER"));
-		user.setRoles(roles);
-		userRepository.save(user);
-	}
-
 	public User findOneWithTeamsByName(String username) {
 		User user = userRepository.findByUserName(username);
 		return findOneWithTeamsById(user.getUserId());
 	}
 
-	public void delete(String id) {
+	public User findUserByVerificationToken(String verificationToken) {
+		return verificationTokenRepository.findByToken(verificationToken).getUser();
+	}
+
+	public User findUserByPasswordResetToken(String token) {
+		return resetTokenRepository.findByToken(token).getUser();
+	}
+
+	public void save(User user) {
+		userRepository.save(user);
+	}
+
+	public void deleteUser(String id) {
 		userRepository.delete(id);
 	}
-	
-	public User findOneByUserName(String username) {
-		return userRepository.findByUserName(username);
+
+	public User registerNewUserAccount(User userInfo) {
+		User user = new User();
+		user.setUserName(userInfo.getUserName());
+		user.setUserPassword(passwordEncoder.encode(userInfo.getUserPassword()));
+		user.setUserEmail(userInfo.getUserEmail());
+		user.setRoles(Arrays.asList(roleRepository.findByRoleName("ROLE_USER")));
+		return userRepository.save(user);
+	}
+
+	public void createVerificationTokenForUser(User user, String token) {
+		VerificationToken myToken = new VerificationToken(token, user);
+		verificationTokenRepository.save(myToken);
+	}
+
+	public VerificationToken getVerificationToken(String VerificationToken) {
+		return verificationTokenRepository.findByToken(VerificationToken);
+	}
+
+	public VerificationToken regenerateVerificationToken(String existingVerificationToken) {
+		VerificationToken vToken = verificationTokenRepository
+				.findByToken(existingVerificationToken);
+		vToken.updateToken(UUID.randomUUID().toString());
+		vToken = verificationTokenRepository.save(vToken);
+		return vToken;
+	}
+
+	public void createPasswordResetTokenForUser(User user, String token) {
+		PasswordResetToken myToken = new PasswordResetToken(token, user);
+		resetTokenRepository.save(myToken);
+	}
+
+	public PasswordResetToken getPasswordResetToken(String token) {
+		return resetTokenRepository.findByToken(token);
+	}
+
+	public void changeUserPassword(User user, String password) {
+		user.setUserPassword(passwordEncoder.encode(password));
+		userRepository.save(user);
+	}
+
+	public boolean validOldPassword(User user, String oldPassword) {
+		return passwordEncoder.matches(oldPassword, user.getUserPassword());
+	}
+
+	public boolean emailExists(String email) {
+		User user = userRepository.findByUserEmail(email);
+		if (user != null) {
+			return true;
+		}
+		return false;
 	}
 }
