60cdbaa62b0b6431c1649610637c365cabf180d1
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-04-29 08:35:56 -0400

    Set up mail and emailService

--- src/main/java/com/kossyuzokwe/fantasy/util/Constants.java
+++ src/main/java/com/kossyuzokwe/fantasy/util/Constants.java
@@ -3,4 +3,9 @@ package com.kossyuzokwe.fantasy.util;
 public class Constants {
 
 	public static final int STANDARD_PAGE_SIZE = 20;
+	public static final String REGISTRATION_EMAIL_SUBJECT = "Registration Confirmation";
+	public static final String REGISTRATION_EMAIL_PREAMBLE = "Your registration was successful. Click the following link to verify your account:";
+	public static final String RESET_EMAIL_SUBJECT = "Reset Your Password";
+	public static final String RESET_EMAIL_PREAMBLE = "You have requested to reset your password. Click the following link to change your password:";
+	public static final String SENDER_EMAIL = "admin@fantasy.kossyuzokwe.com";
 }
