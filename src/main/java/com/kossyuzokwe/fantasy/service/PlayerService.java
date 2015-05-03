8c2e835ecd34cf55de77029eb2aed4e3c5c2782d
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-05-03 14:05:08 -0400

    Cleaned up PlayerService
    Removed unused repository fields and added a transactional annotation to
    the getPlayers method

--- src/main/java/com/kossyuzokwe/fantasy/service/PlayerService.java
+++ src/main/java/com/kossyuzokwe/fantasy/service/PlayerService.java
@@ -2,15 +2,14 @@ package com.kossyuzokwe.fantasy.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kossyuzokwe.fantasy.dao.LeagueRepository;
 import com.kossyuzokwe.fantasy.dao.PlayerRepository;
-import com.kossyuzokwe.fantasy.dao.TeamRepository;
-import com.kossyuzokwe.fantasy.dao.UserRepository;
 import com.kossyuzokwe.fantasy.model.Player;
 
 @Service
@@ -19,17 +18,9 @@ public class PlayerService {
 	Logger LOGGER = LoggerFactory.getLogger(getClass());
 
 	@Autowired
-	private TeamRepository teamRepository;
-
-	@Autowired
 	private PlayerRepository playerRepository;
 
-	@Autowired
-	private LeagueRepository leagueRepository;
-
-	@Autowired
-	private UserRepository userRepository;
-
+	@Transactional
 	public List<Player> getPlayers() {
 		return playerRepository.findAll();
 	}
